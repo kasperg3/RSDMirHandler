e9fe74567c9149149410c07609a035b1bf0c0b9a
kasperg3 <kasperg3@rocketmail.com>
2019-12-12 21:59:32 +0100

    added working stuff

--- main.py
+++ main.py
@@ -1,67 +1,82 @@
 import requests
 
 import kivy
-
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-#Probable will change after network is ready
-url = 'http://10.10.19.40/api/v2.0.0/'
-
-Authorization = {
-    'Authorization': "PUT_THE_RIGHT_KEY"}
-language = {'Accept-Language': "en_US"}
 
 
 class RestMiR():
     def __init__(self):
-        self.authorization = {
-            'Authorization': "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=="}
+        # Probable will change after network is ready
+        self.url = 'http://10.10.19.40/api/v2.0.0/'
+        language = {'Accept-Language': "en_US"}
+        self.authorization = {'Authorization': "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=="}
         self.HOST = 'http://10.10.19.40/api/v2.0.0/'
 
-    #In the mission we will have to set coils (plc registers) in order to get information if robot has docked and etc.
     def read_register(self, register_id):
         try:
-            response = requests.get(url + 'registers/' + str(register_id), headers=self.authorization)
+            response = requests.get(self.url + 'registers/' + str(register_id), headers=self.authorization)
         except requests.exceptions.ConnectionError:
             return
 
         register_value = 0
-        if response.status_code != 200:
-            print(response.status_code)
-            print(response.text)
 
         if (response.json()['id'] == register_id):
             register_value = response.json()['value']
         return register_value
 
-
-    #As above, we can set the register when loading on the robot is ready to move to main table
     def write_register(self, register_id, value):
         data = {"value": value}
-        response = requests.put(url + 'registers/' + str(register_id), json = data, headers=self.authorization)
-        if response.status_code != 200:
-            print(response.status_code)
-        return 0
+        try:
+            requests.put(self.url + 'registers/' + str(register_id), json = data, headers=self.authorization)
+        except requests.exceptions.ConnectionError:
+            return
+        return
 
 # class in which we are creating the button
 class ButtonApp(App):
     mir = RestMiR()
     def build(self):
-        btn = Button(text="Release MIR",
-                     font_size="50sp",
-                     background_color=(1, 1, 1, 1),
-                     color=(1, 1, 1, 1))
+        self.btn = Button(text="Release MIR",
+                          font_size="50sp",
+                          background_color=(1, 1, 1, 1),
+                          color=(1, 1, 1, 1), size_hint=(1,1))
+
+        self.btn2 = Button(text="Register 150 Value: ",
+                           font_size="20sp",
+                           background_color=(1, 1, 1, 1),
+                           color=(1, 1, 1, 1), size_hint=(0.5,1))
+
+        self.btn3 = Button(text="Charging: ",
+                           font_size="20sp",
+                           background_color=(1, 1, 1, 1),
+                           color=(1, 1, 1, 1), size_hint=(0.5,1))
 
         # bind() use to bind the button to function callback
-        btn.bind(on_release=self.callback)
-        return btn
+        self.btn.bind(on_release=self.callback)
+        self.btn2.bind(on_release=self.callback2)
+        self.btn3.bind(on_release=self.callback3)
+        self.layout1 = BoxLayout(orientation='horizontal', size_hint=(1,0.2))
+        self.layout1.add_widget(self.btn2)
+        self.layout1.add_widget(self.btn3)
+        layout = BoxLayout(orientation='vertical', size_hint=(1,1))
+        layout.add_widget(self.btn)
+        layout.add_widget(self.layout1)
+
+        return layout
 
     def callback(self, event):
-        #Release the mir
-        #value = self.mir.read_register(150)
-        #self.mir.write_register(150, not value)
         self.mir.write_register(150, 1)
-        print("Register 150 Value: " + str(self.mir.read_register(150)))
+
+    def callback2(self, event):
+        self.btn2.text = "Register 150 Value: " + str(self.mir.read_register(150))
+
+    def callback3(self, event):
+        if self.mir.read_register(110) == 0:
+            self.btn3.text = "Charging: False"
+        else:
+            self.btn3.text = "Charging: True"
 
 
 root = ButtonApp()
